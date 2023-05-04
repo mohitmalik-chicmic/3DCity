47c5acc48e4bbbdce63edfc1714dc4e7285592b7
Mohit Malik <mohit.malik@chicmic.co.in>
2023-05-04 13:52:42 +0530

    collision and smooth player movement

--- assets/CameraPos.ts
+++ assets/CameraPos.ts
@@ -1,4 +1,5 @@
-import { _decorator, Canvas, Component, Input, input, Node, v3 } from 'cc';
+import { _decorator, Canvas, Component, Input, input, Node, v3, Vec3 } from 'cc';
+import { manRotate } from './manRotate';
 const { ccclass, property } = _decorator;
 
 @ccclass('CameraPos')
@@ -11,10 +12,13 @@ export class CameraPos extends Component {
     //     const mousePos = event.getLocation();
 
     // }
-
+    @property({ type: Node })
+    manRotate: Node = null;
+    mouseeX
+    pos = new Vec3(0, 0, 0)
     start() {
 
-        input.on(Input.EventType.MOUSE_MOVE, function (event) {
+        input.on(Input.EventType.TOUCH_MOVE, function (event) {
             // var mousePos = this.node.parent
             //     .getComponent(UITransform)
             //     .convertToNodeSpaceAR(
@@ -23,20 +27,26 @@ export class CameraPos extends Component {
             var mousePos = event.getLocation();
             mousePos.x
             this.mousex = mousePos.x;
-
+            this.mouseeX = this.mousex
             this.mousey = mousePos.y;
             this.mousez = mousePos.x
             //  this.mousey.convertToNodeSpaceAR(event.getLocation().x, event.getLocation().y)
 
-            this.node.eulerAngles = v3(0, this.mousex, 0);
+            this.node.eulerAngles = v3(this.node.eulerAngles.x, this.mousex, this.node.eulerAngles.z);
             // this.node.setPosition(new Vec3(0, 0, this.mousey));
-            console.log(this.mousex, "mouseX", this.mousey, "mouseY")
+            // console.log(this.mousex, "mouseX", this.mousey, "mouseY")
         }, this);
     }
-
+    addDifference(manNode) {
+        this.pos.x = (manNode.getPosition().x - this.node.getPosition().x) / 20;
+        this.pos.y = manNode.getPosition().y - this.node.getPosition().y;
+        this.pos.z = (manNode.getPosition().z - this.node.getPosition().z) / 20;
+        let p = this.node.getPosition()
+        this.node.setPosition(new Vec3(p.x + this.pos.x, p.y, p.z + this.pos.z))
+    }
 
     update(deltaTime: number) {
-
+        // this.manRotate.getComponent(manRotate).Time(this.mouseeX)
     }
 }
 
--- assets/manRotate.ts
+++ assets/manRotate.ts
@@ -1,4 +1,6 @@
-import { _decorator, Component, EventKeyboard, input, Input, macro, Node, Quat, tween, UITransform, v3, Vec3 } from 'cc';
+import { _decorator, Component, EventKeyboard, input, Input, macro, Node, Quat, SkeletalAnimation, tween, UITransform, v3, Vec3 } from 'cc';
+import { Collision } from './Collision';
+import { CameraPos } from './CameraPos';
 const { ccclass, property } = _decorator;
 enum KEY {
     SPACE = 32,
@@ -8,56 +10,110 @@ enum KEY {
 }
 @ccclass('manRotate')
 export class manRotate extends Component {
+
     changeY = 0
     changePos: Vec3 = null;
-    @property({ type: Node })
-    camera: Node = null;
+    // @property({ type: Node })
+    // camearaNode: Node = null;
     deltaTime: number;
     deltatime: number;
-    speed = 20
+    speed = 5
+    playerMove: boolean;
+    CharacterPosition: Vec3;
+    pos: any;
+    moveCharacter: boolean;
     start() {
-        input.on(Input.EventType.MOUSE_MOVE, function (event) {
+        input.on(Input.EventType.TOUCH_MOVE, function (event) {
             var mousePos = event.getLocation();
             mousePos.x
             this.mousex = mousePos.x;
-
             this.mousey = mousePos.y;
             this.mousez = mousePos.x
             this.node.eulerAngles = v3(0, this.mousex, 0);
-            console.log(this.mousex, "mouseX", this.mousey, "mouseY")
+            // if (this.mousey < 135)
+            //     this.node.eulerAngles = v3(this.mousey, this.mousex, 0);
+            // else
+            //     this.node.eulerAngles = v3(this.node.eulerAngles.x, this.mousex, 0);
+            // console.log(this.mousex, "mouseX", this.mousey, "mouseY")
         }, this);
         this.changePos = this.node.getPosition();
         input.on(Input.EventType.KEY_PRESSING, this.onKeyPressing, this)
+        input.on(Input.EventType.KEY_UP, this.onKeyUp, this)
     }
-
     onKeyPressing(event: EventKeyboard) {
+
         switch (event.keyCode) {
             case 38: {
-                // this.changePos.z += 2
                 console.log("UP Pressed");
-                let CharacterPosition = new Vec3();
-                let Destination = new Vec3();
-
-                Destination.x =
-                    this.node.getPosition().x +
-                    this.node.forward.x
-                Destination.y = this.node.getPosition().y;
-                Destination.z =
-                    this.node.getPosition().z +
-                    this.node.forward.z
-                Vec3.lerp(
-                    CharacterPosition,
-                    this.node.getPosition(),
-                    Destination,
-                    0.5
-                );
-
-                this.node.setPosition(CharacterPosition);
+                // this.node.eulerAngles = v3(0, this.pos, 0)
+                this.moveCharacter = true
+                // this.characterPosition()
             }
+                break;
         }
     }
-    update(deltaTime: number) {
-        this.deltatime = deltaTime
+    onKeyUp(event: EventKeyboard) {
+
+        switch (event.keyCode) {
+            case 38: {
+                console.log("UP Pressed");
+                // this.node.eulerAngles = v3(0, this.pos, 0)
+                this.moveCharacter = false
+                // this.characterPosition()
+            }
+                break;
+        }
     }
+
+    characterPosition() {
+        // this.CharacterPosition = new Vec3();
+        // let Destination = new Vec3();
+        // Destination.x =
+        //     this.node.getPosition().x +
+        //     this.node.forward.x
+        // Destination.y = this.node.getPosition().y;
+        // Destination.z =
+        //     this.node.getPosition().z +
+        //     this.node.forward.z
+        // Vec3.lerp(
+        //     this.CharacterPosition,
+        //     this.node.getPosition(),
+        //     Destination,
+        //     0.5
+        // );
+        // this.node.setPosition(this.CharacterPosition);
+        // this.camearaNode.getComponent(CameraPos).addDifference(this.node)
+        // else {
+
+        // }
+    }
+    // exitCollision() {
+    //     let pos = this.node.getPosition();
+
+    // }
+    update(deltaTime: number) {
+        if (this.moveCharacter) {
+            this.deltatime = deltaTime
+            this.CharacterPosition = new Vec3();
+            let Destination = new Vec3();
+            Destination.x =
+                this.node.getPosition().x +
+                this.node.forward.x * deltaTime * this.speed
+            Destination.y = this.node.getPosition().y;
+            Destination.z =
+                this.node.getPosition().z +
+                this.node.forward.z * deltaTime * this.speed
+            Vec3.lerp(
+                this.CharacterPosition,
+                this.node.getPosition(),
+                Destination,
+                0.5
+            );
+            this.node.setPosition(this.CharacterPosition);
+        }
+    }
+    // Time(node) {
+    //     this.pos = node
+    // }
 
 }
